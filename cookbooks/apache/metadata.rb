78579854c0fe35e1ca6fc1dcdb32bb2d4440ff5c
root <root@jackiechute1.mylabserver.com>
2018-03-26 15:05:41 +0000

    added motd and changed apache default for node attributes

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jackie.chute@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures apache'
 long_description 'Installs/Configures apache'
-version '0.1.0'
+version '0.2.1'
 chef_version '>= 12.14' if respond_to?(:chef_version)
 
 %w(amazon centos redhat oracle ubuntu).each do |os|
